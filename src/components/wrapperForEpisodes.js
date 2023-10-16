42ac40792292dcea8faffee29ded7229f0d9f06b
Zorenko Elena <75756981+ZorenkoElena@users.noreply.github.com>
2023-10-17 00:02:50 +0300

    Revert "fixed bugs"
    This reverts commit fe303049c1439cb806216396ec74cc7ec212e4df.

--- src/components/wrapperForEpisodes.js
+++ src/components/wrapperForEpisodes.js
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
 import './component.css';
 import useAxiosEpisodes from '../hooks/useAxiosEpisodes.js';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Pagination from './pagination';
 import HeroCard from './card.js';
 
 const ListOfEpisodes = (props) => {
-  const navigate = useNavigate();
-  const showCardInfo = (id) => navigate('/episodes');
   return (
     <div className="herolist">
       {props.data.map((hero, index) => (
-        <HeroCard key={`hero-card-${index}`} onClick={() => showCardInfo()}>
-          {hero.name}
-        </HeroCard>
+        <HeroCard key={`hero-card-${index}`}>{hero.name}</HeroCard>
       ))}
     </div>
   );
